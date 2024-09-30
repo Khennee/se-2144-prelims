6d4cb55453fe6fd0dc0bbc677f4968e18604e9af
Khynie <Shawnsevalair@gmail.com>
2024-09-30 18:36:46 +0800

    color changes

--- script.js
+++ script.js
@@ -33,19 +33,22 @@ function displayNumber(number) {
   if (displayValue.length < 20) {
     displayValue += number;
     display.value = displayValue;
-    if (displayValue.length >= 15) {
-      display.style.fontSize = '20px';
-    } 
-    else if (displayValue.length >= 10) {
-      display.style.fontSize = '30px';
-    }
-    else if (displayValue.length >= 5) {
-      display.style.fontSize = '40px';
-    } else {
-      display.style.fontSize = '50px';
-    }
+    adjustFontSize(displayValue.length);
   }
 }
+
+function adjustFontSize(length) {
+  if (length >= 15) {
+    display.style.fontSize = '20px';
+  } else if (length >= 10) {
+    display.style.fontSize = '30px';
+  } else if (length >= 5) {
+    display.style.fontSize = '40px';
+  } else {
+    display.style.fontSize = '50px'; // Reset to original size if length is less than 5
+  }
+}
+
 
 function clearDisplay() {
   displayValue = '';
@@ -56,14 +59,12 @@ function helloFeature(){
   const languageKeys = Object.keys(helloMessages);
   const randomLanguageKey = languageKeys[Math.floor(Math.random() * languageKeys.length)];
   display.value = helloMessages[randomLanguageKey];
+
   
   const buttons = document.querySelectorAll('button');
   buttons.forEach(button => button.disabled = true);
   
-  setTimeout(function() {
-    display.value = '';
-    buttons.forEach(button => button.disabled = false);
-  }, 1000);
+  helloButton.disabled = false;
 }
 
 helloButton.addEventListener('click', function () {
@@ -81,6 +82,19 @@ function startCalculator() {
 
   ACButton.style.backgroundColor = '#8BC34A'
   display.style.backgroundColor = '#FFC5C5'
+  offButton.style.backgroundColor = '#C65765'
+  decimal.style.backgroundColor = '#FFC5C5'
+  backspaceButton.style.backgroundColor = '#A879C4'
+  addButton.style.backgroundColor = '#A879C4'
+  subtractButton.style.backgroundColor = '#A879C4'
+  multiplyButton.style.backgroundColor = '#A879C4'
+  divideButton.style.backgroundColor = '#A879C4'
+  equalButton.style.backgroundColor = '#8BC34A'
+  helloButton.style.backgroundColor = '#F7DC6F'
+  for (let i = 0; i <= 9; i++) {
+    const button = document.getElementById(i.toString());
+    button.style.backgroundColor = '#FFC5C5';
+  }
 
   ACButton.disabled = false;
   ACButton.value = 'AC';
@@ -93,7 +107,19 @@ function shutdownCalculator() {
 
   ACButton.style.backgroundColor = '#C65765'
   display.style.backgroundColor = 'rgb(130, 130, 130)'
-  display.style.boxShadow = '0 0 10px'
+  offButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  decimal.style.backgroundColor = 'rgb(130, 130, 130)'
+  backspaceButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  addButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  subtractButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  multiplyButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  divideButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  equalButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  helloButton.style.backgroundColor = 'rgb(130, 130, 130)'
+  for (let i = 0; i <= 9; i++) {
+    const button = document.getElementById(i.toString());
+    button.style.backgroundColor = 'rgb(130, 130, 130)';
+  }
 
   const buttons = document.querySelectorAll('button');
   buttons.forEach(button => button.disabled = true);
@@ -118,8 +144,10 @@ for (let i = 0; i <= 9; i++) {
   const button = document.getElementById(i.toString());
   button.addEventListener('click', function() {
     if (isCalculatorOn) {
-      if (lastOperation === '=') { 
-        clearDisplay(); 
+      if (lastOperation === '=') {
+        if (!isOperator(displayValue[displayValue.length - 1])) {
+          clearDisplay();
+        }
       }
       displayNumber(i);
       lastOperation = ''; 
@@ -128,11 +156,12 @@ for (let i = 0; i <= 9; i++) {
 }
 
 function backSpace() {
-    if (displayValue.length > 0) {
-      displayValue = displayValue.slice(0, -1);
-      display.value = displayValue;
-    }
+  if (displayValue.length > 0) {
+    displayValue = displayValue.slice(0, -1);
+    display.value = displayValue;
+    adjustFontSize(displayValue.length); // Adjust font size based on new length
   }
+}
 
 backspaceButton.addEventListener('click', backSpace)
 
@@ -150,61 +179,45 @@ let subtract = '-';
 let multiply = '×';
 let divide = '÷';
 function addOperator() {
-  if (displayValue.length > 0 && lastOperation !== '=') {
-    if (isOperator(displayValue[displayValue.length - 1])) {
-      display.value = "Error";
-    } else {
-      displayValue += add;
-      display.value = displayValue;
-    }
+  if (displayValue.length > 0 && !isOperator(displayValue[displayValue.length - 1])) {
+    displayValue += '+';
+    display.value = displayValue;
   } else if (lastOperation === '=') {
-    displayValue = displayValue + add;
+    displayValue += '+';
     display.value = displayValue;
     lastOperation = ''; 
   }
 }
 
-function subtractOperator(){
-  if (displayValue.length > 0 && lastOperation !== '=') {
-    if (isOperator(displayValue[displayValue.length - 1])) {
-      display.value = "Error";
-    } else {
-      displayValue += subtract;
-      display.value = displayValue
-    }
+function subtractOperator() {
+  if (displayValue.length > 0 && !isOperator(displayValue[displayValue.length - 1])) {
+    displayValue += '-';
+    display.value = displayValue;
   } else if (lastOperation === '=') {
-    displayValue = displayValue + subtract;
-    display.value = displayValue
+    displayValue += '-';
+    display.value = displayValue;
     lastOperation = ''; 
   }
 }
 
-function multiplyOperator(){ 
-  if (displayValue.length > 0 && lastOperation !== '=') {
-    if (isOperator(displayValue[displayValue.length - 1])) {
-      display.value = "Error";
-    } else {
-      displayValue += multiply;
-      display.value = displayValue
-    }
+function multiplyOperator() {
+  if (displayValue.length > 0 && !isOperator(displayValue[displayValue.length - 1])) {
+    displayValue += '×';
+    display.value = displayValue;
   } else if (lastOperation === '=') {
-    displayValue = displayValue + multiply;
-    display.value = displayValue
+    displayValue += '×';
+    display.value = displayValue;
     lastOperation = ''; 
   }
 }
 
-function divideOperator(){ 
-  if (displayValue.length > 0 && lastOperation !== '=') {
-    if (isOperator(displayValue[displayValue.length - 1])) {
-      display.value = "Error";
-    } else {
-      displayValue += divide; 
-      display.value = displayValue
-    }
+function divideOperator() {
+  if (displayValue.length > 0 && !isOperator(displayValue[displayValue.length - 1])) {
+    displayValue += '÷';
+    display.value = displayValue;
   } else if (lastOperation === '=') {
-    displayValue = displayValue + divide; 
-    display.value = displayValue
+    displayValue += '÷';
+    display.value = displayValue;
     lastOperation = ''; 
   }
 }
@@ -213,40 +226,28 @@ function isOperator(char) {
   return char === '+' || char === '-' || char === '×' || char === '÷';
 }
 
-
 function calculate() {
-  let previousValue = displayValue; // Store the previous value
-  if (isOperator(displayValue[displayValue.length - 1])) {
-    displayValue = "Error!";
-    display.value = displayValue;
-    setTimeout(function() {
-      displayValue = previousValue; // Restore the previous value
-      display.value = displayValue;
-      lastOperation = ''; // Reset lastOperation
-    }, 1000);
-  } else {
-    let numbers = displayValue.split(/[-+×÷]/);
-    let operators = displayValue.match(/[-+×÷]/g);
-    let result = parseFloat(numbers[0]);
 
-    for (let i = 0; i < operators.length; i++) {
-      if (operators[i] === '+') {
-        result += parseFloat(numbers[i + 1]);
-      } else if (operators[i] === '-') {
-        result -= parseFloat(numbers[i + 1]);
-      } else if (operators[i] === '×') {
-        result *= parseFloat(numbers[i + 1]);
-      } else if (operators[i] === '÷') {
-        result /= parseFloat(numbers[i + 1]);
-      }
+  let numbers = displayValue.split(/[-+×÷]/);
+  let operators = displayValue.match(/[-+×÷]/g);
+  let result = parseFloat(numbers[0]);
+
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === '+') {
+      result += parseFloat(numbers[i + 1]);
+    } else if (operators[i] === '-') {
+      result -= parseFloat(numbers[i + 1]);
+    } else if (operators[i] === '×') {
+      result *= parseFloat(numbers[i + 1]);
+    } else if (operators[i] === '÷') {
+      result /= parseFloat(numbers[i + 1]);
     }
 
     displayValue = result.toString();
     display.value = displayValue;
   }
-  lastOperation = '='
+  lastOperation = '='; 
 }
-
 
 equalButton.addEventListener('click', calculate);
 addButton.addEventListener('click', addOperator);
